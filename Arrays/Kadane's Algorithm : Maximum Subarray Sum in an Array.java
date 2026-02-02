349d1143e85a06de1881bbc0b9334829f3699bce
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2026-02-02 15:26:52 +0530

    Create Kadane's Algorithm : Maximum Subarray Sum in an Array.java

---
+++ Arrays/Kadane's Algorithm : Maximum Subarray Sum in an Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        long maxSum = Long.MIN_VALUE;
+        long currSum = 0;
+
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            if(currSum > maxSum)
+                maxSum = currSum;
+            if(currSum < 0)
+                currSum = 0;
+        }
+        return (int)maxSum;
+
+    }
+}
