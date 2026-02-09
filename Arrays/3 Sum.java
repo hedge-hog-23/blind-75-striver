17100b14784ba5e7137997b1110913104beec4ea
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2026-02-09 14:59:58 +0530

    Create 3 Sum.java

---
+++ Arrays/3 Sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            int l = i+1;
+            int r = n-1;
+
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0){
+                    arr.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+
+                    //skipping dups
+                    while(l < r && nums[l] == nums[l-1]) l++;
+                    while(l < r && nums[r] == nums[r+1]) r--;
+                }
+
+                else if(sum < 0)
+                    l++;
+                else
+                    r--;
+
+            }
+        }
+        return arr;
+
+    }
+}
